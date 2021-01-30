553e7cac09e7c1681f2eab97cd361ba54d16f81a
Renan <renan.baialuna@gmail.com>
2021-01-30 00:00:08 -0300

    Initial Commit

---
+++ DeliveriesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeliveriesApp
+//
+//  Created by Renan Baialuna on 30/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
